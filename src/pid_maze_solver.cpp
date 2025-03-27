80bb267ddd45568cb6adec652025f6f838813f62
Kailash Khadka <k.khadka343@gmail.com>
2025-03-27 15:43:21 +0000

    Updated PID controller class

--- src/pid_maze_solver.cpp
+++ src/pid_maze_solver.cpp
@@ -1,10 +1,18 @@
+#include "ament_index_cpp/get_package_share_directory.hpp"
+#include "ament_index_cpp/get_package_share_directory.hpp" // Include this header
+#include "yaml-cpp/yaml.h" // include the yaml library
+#include <Eigen/Dense>
+#include <filesystem> // Include the filesystem library
 #include <geometry_msgs/msg/twist.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/float32_multi_array.hpp>
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
+#include <unordered_map>
 #include <vector>
+
+using namespace std::chrono_literals;
 
 class PID {
 public:
@@ -13,28 +21,25 @@ public:
   PID(double kp, double ki, double kd, double dt)
       : kp_(kp), ki_(ki), kd_(kd), dt_(dt), integral_(0.0), prev_error_(0.0) {}
 
-  double compute(double setpoint, double measurement) {
-    double error = setpoint - measurement;
+  double compute(double error) {
     integral_ += error * dt_;
+    integral_ = std::clamp(integral_, -0.5, 0.5); // Anti-windup
     double derivative = (error - prev_error_) / dt_;
     double output = kp_ * error + ki_ * integral_ + kd_ * derivative;
-
     prev_error_ = error;
     return output;
   }
 
-  double getError() { return prev_error_; }
+  void reset_() {
+    integral_ = 0.0;
+    prev_error_ = 0.0;
+  }
 
 private:
-  double kp_;
-  double ki_;
-  double kd_;
-  double dt_;
+  double kp_, ki_, kd_, dt_;
   double integral_;
   double prev_error_;
 };
-
-using namespace std::chrono_literals;
 
 class PIDMazeSolver : public rclcpp::Node {
 private:
@@ -42,9 +47,12 @@ private:
   double max_velocity_;
   double max_ang_velocity_;
   std::vector<std::vector<double>> waypoints_; //{dx, dy, dphi}
+  std::unordered_map<std::string, std::vector<float>> motions;
 
-  void SelectWaypoints();
+  void readWaypointsYAML();
   void pid_controller();
+  std::vector<double> global2localvelocity(double vx, double vy, double avz);
+  std::vector<double> velocity2wheelspeed(double wz, double vx, double vy);
 
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_subscriber_;
@@ -60,6 +68,18 @@ private:
   PID pid_x_, pid_y_, pid_z_;
   double time_step = 0.01; // in milliseconds
 
+  // Robot parameters
+  double l;
+  double r;
+  double w;
+
+  // Transformation matrix H_4x3
+  Eigen::MatrixXd H;
+  // Pseudo-inverse of H
+  Eigen::MatrixXd H_pseudo_inverse;
+  // vector wheel speeds
+  Eigen::Vector4d u;
+
 public:
   PIDMazeSolver(int scene_number);
   ~PIDMazeSolver();
@@ -67,7 +87,16 @@ public:
 };
 
 PIDMazeSolver::~PIDMazeSolver() {
-  RCLCPP_INFO(this->get_logger(), "Turn Controller Terminated.");
+  RCLCPP_INFO(this->get_logger(), "Maze Solver Terminated.");
+}
+
+// Function to normalize angle to the range -pi to pi
+double normalize_angle(double angle) {
+  while (angle > M_PI)
+    angle -= 2.0 * M_PI;
+  while (angle < -M_PI)
+    angle += 2.0 * M_PI;
+  return angle;
 }
 
 void PIDMazeSolver::odom_callback(
@@ -82,7 +111,7 @@ void PIDMazeSolver::odom_callback(
   // Convert quaternion to Euler angles (roll, pitch, yaw)
   double roll, pitch, yaw;
   tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
-  phi = yaw;
+  phi = normalize_angle(yaw);
 
   // Log the position and orientation for debugging
   RCLCPP_DEBUG(this->get_logger(),
@@ -93,7 +122,15 @@ void PIDMazeSolver::odom_callback(
 
 PIDMazeSolver::PIDMazeSolver(int scene_number)
     : Node("maze_solver_node"), scene_number_(scene_number) {
-
+  motions = {
+      {"forward", {1.0, 1.0, 1.0, 1.0}},            // All wheels forward
+      {"backward", {-1.0, -1.0, -1.0, -1.0}},       // All wheels backward
+      {"left", {-1.0, 1.0, -1.0, 1.0}},             // Sideways left
+      {"right", {1.0, -1.0, 1.0, -1.0}},            // Sideways right
+      {"clockwise", {1.0, -1.0, -1.0, 1.0}},        // Clockwise rotation
+      {"counterclockwise", {-1.0, 1.0, 1.0, -1.0}}, // Counter-clockwise
+      {"stop", {0.0, 0.0, 0.0, 0.0}}                // Stop all wheels
+  };
   timer_cb_grp_ =
       this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
   odom_cb_grp_ =
@@ -108,25 +145,67 @@ PIDMazeSolver::PIDMazeSolver(int scene_number)
       std::bind(&PIDMazeSolver::odom_callback, this, std::placeholders::_1),
       options);
 
-  SelectWaypoints();
-  /* https://husarion.com/manuals/rosbot-xl/
-  Maximum translational velocity = 0.8 m/s
-  Maximum rotational velocity = 180 deg/s (3.14 rad/s)
-  */
-  max_velocity_ = 0.8;
-  max_ang_velocity_ = 3.14;
-  pid_x_ = PID(1.0, 0.01, 0.20, time_step);
-  pid_y_ = PID(1.0, 0.01, 0.20, time_step);
-  pid_z_ = PID(2.0, 0.01, 0.30, time_step);
+  // Based on simulation or real bot
+  // Read waypoints from YAML file & update waypoints vector
+  // And configure PID controllers
+  readWaypointsYAML();
+
+  // Robot parameters
+  l = 0.170 / 2; // Half of the wheel base distance
+  r = 0.100 / 2; // Radius of the wheels
+  w = 0.270 / 2; // Half of track width
+
+  // Initialize the transformation matrix H_4x3 with the correct size
+  H = Eigen::MatrixXd(4, 3);                   // Explicitly size the matrix
+  H.row(0) << (-l - w) / r, 1.0 / r, -1.0 / r; // Row 0
+  H.row(1) << (l + w) / r, 1.0 / r, 1.0 / r;   // Row 1
+  H.row(2) << (l + w) / r, 1.0 / r, -1.0 / r;  // Row 2
+  H.row(3) << (-l - w) / r, 1.0 / r, 1.0 / r;  // Row 3
+
+  // Compute the pseudo-inverse of H
+  H_pseudo_inverse = H.completeOrthogonalDecomposition().pseudoInverse();
+
   RCLCPP_INFO(this->get_logger(), "Maze Solver Initialized.");
 
   timer_ = this->create_wall_timer(
       1s, std::bind(&PIDMazeSolver::pid_controller, this), timer_cb_grp_);
 }
 
+std::vector<double> PIDMazeSolver::velocity2wheelspeed(double wz, double vx,
+                                                       double vy) {
+  // Twist vector
+  Eigen::Vector3d twist(wz, vx, vy);
+
+  // Compute wheel speeds: u = H * twist
+  Eigen::Vector4d u = H * twist;
+
+  // Convert Eigen vector to std::vector<double>
+  std::vector<double> wheel_speeds(u.data(), u.data() + u.size());
+
+  return wheel_speeds;
+}
+
+std::vector<double> PIDMazeSolver::global2localvelocity(double vx, double vy,
+                                                        double avz) {
+  // Create input vector
+  Eigen::Vector3d velocity(vx, vy, avz);
+
+  // Define the transformation matrix R row-wise
+  Eigen::MatrixXd R(3, 3);                      // 3x3 matrix
+  R.row(0) << 1, 0, 0;                          // Row 0
+  R.row(1) << 0, std::cos(phi), std::sin(phi);  // Row 1
+  R.row(2) << 0, -std::sin(phi), std::cos(phi); // Row 2
+
+  // Perform matrix-vector multiplication
+  Eigen::Vector3d twist = R * velocity;
+
+  // Convert Eigen::Vector3d to std::vector<double>
+  return std::vector<double>{twist(0), twist(1), twist(2)};
+}
 void PIDMazeSolver::pid_controller() {
   double u_x, u_y, u_z;
   std::vector<double> capped_velocities;
+  std::vector<double> wheel_speeds, u;
   double dx, dy, dphi;
   double sp_x, sp_y, sp_phi;
   double error, distance;
@@ -134,16 +213,35 @@ void PIDMazeSolver::pid_controller() {
   RCLCPP_INFO(this->get_logger(), "Trajectory started.");
 
   // Loop through each waypoint
+  int index = 0;
   for (const auto &waypoint : waypoints_) {
 
     dx = waypoint[0];
     dy = waypoint[1];
     dphi = waypoint[2];
-    RCLCPP_INFO(this->get_logger(), "WP: [%.2f, %.2f, %.2f]", dx, dy, dphi);
+    RCLCPP_INFO(this->get_logger(), "WP%u: [%.2f, %.2f, %.2f]", ++index, dx, dy,
+                dphi);
 
-    sp_x = current_position_.x + dx;
-    sp_y = current_position_.y + dy;
-    sp_phi = phi + dphi;
+    // 1. Change to local waypoints wrt Robot frame
+    auto local_vels = global2localvelocity(dphi, dx, dy);
+    // 2. Compute the wheel speeds
+    auto wheel_speeds =
+        velocity2wheelspeed(local_vels[0], local_vels[1], local_vels[2]);
+    // Extract wheel speeds (in rad/s) and assign to vector u
+
+    u << wheel_speeds[0], wheel_speeds[1], wheel_speeds[2], wheel_speeds[3];
+    // 3. Compute the twist vector
+    Eigen::Vector3d twist_v = H_pseudo_inverse * u;
+
+    // Create and publish Twist message
+    geometry_msgs::msg::Twist twist;
+    twist.linear.x = twist_v(1);
+    twist.linear.y = twist_v(2);
+    twist.angular.z = twist_v(0);
+
+    sp_x = dx;
+    sp_y = dy;
+    sp_phi = normalize_angle(dphi);
 
     rclcpp::Rate rate(int(1 / time_step)); // Control loop frequency
 
@@ -159,13 +257,18 @@ void PIDMazeSolver::pid_controller() {
         rclcpp::shutdown();
         return;
       }
-      // PID calculation for Angle
-      u_z = pid_z_.compute(sp_phi, phi);
 
-      // Calculate error to the target
-      error = pid_z_.getError();
-      RCLCPP_DEBUG(this->get_logger(), "phi: %.3f, angle to target: %.3f rads",
-                   phi, error);
+      // Calculate error for yaw, accounting for wrap-around
+      double error_yaw = normalize_angle(sp_phi - phi);
+
+      // PID calculation for Angle
+      u_z = pid_z_.compute(sp_phi, phi, true);
+
+      // Use the normalized error for logging and control
+      error = error_yaw;
+      RCLCPP_DEBUG(this->get_logger(),
+                   "phi: %.3f, sp_phi, %.3f, target: %.3f rad", phi, sp_phi,
+                   error);
       RCLCPP_DEBUG(this->get_logger(), "Position: %.3f,%.3f,%.3f",
                    current_position_.x, current_position_.y, phi);
 
@@ -177,6 +280,7 @@ void PIDMazeSolver::pid_controller() {
 
       rate.sleep();                // Maintain loop frequency
     } while (fabs(error) > 0.007); // Run until error is within tolerance
+    pid_z_.reset_();
 
     // 2. Move
     distance = 0.0;
@@ -191,21 +295,24 @@ void PIDMazeSolver::pid_controller() {
       // PID calculation
       u_x = pid_x_.compute(sp_x, current_position_.x);
       u_y = pid_y_.compute(sp_y, current_position_.y);
-      u_z = pid_z_.compute(sp_phi, phi);
+      u_z = pid_z_.compute(sp_phi, phi, true);
 
       // Calculate distance to the target
       distance = std::sqrt(std::pow(pid_x_.getError(), 2) +
                            std::pow(pid_y_.getError(), 2));
-      RCLCPP_DEBUG(this->get_logger(), "Distance to target: %.2f", distance);
-      RCLCPP_DEBUG(this->get_logger(), "Position: %.2f,%.2f,%.2f", u_x, u_y,
-                   u_z);
 
       // Prepare and publish the twist message
-      capped_velocities = cap_velocities(u_x, u_y, u_z);
+      auto twist_v = global2localvelocity(u_z, u_x, u_y);
+      capped_velocities = cap_velocities(twist_v[1], twist_v[2], twist_v[0]);
       twist.linear.x = capped_velocities[0];
       twist.linear.y = capped_velocities[1];
       twist.angular.z = capped_velocities[2];
       cmd_vel_publisher_->publish(twist);
+      RCLCPP_DEBUG(this->get_logger(), "Distance to target: %.2f", distance);
+      RCLCPP_DEBUG(this->get_logger(), "Vel: %.2f,%.2f,%.2f", u_x, u_y, u_z);
+      RCLCPP_DEBUG(this->get_logger(), "Twist: %.2f,%.2f,%.2f",
+                   capped_velocities[0], capped_velocities[1],
+                   capped_velocities[2]);
 
       rate.sleep();            // Maintain loop frequency
     } while (distance > 0.01); // Run until distance is within tolerance
@@ -217,6 +324,11 @@ void PIDMazeSolver::pid_controller() {
     twist.angular.z = 0.0;
     cmd_vel_publisher_->publish(twist);
     std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // Reset PID for new waypoints
+    pid_x_.reset_();
+    pid_y_.reset_();
+    pid_z_.reset_();
   }
 
   RCLCPP_INFO(this->get_logger(), "Trajectory completed.");
@@ -225,48 +337,101 @@ void PIDMazeSolver::pid_controller() {
   rclcpp::shutdown();
 }
 
-void PIDMazeSolver::SelectWaypoints() {
+void PIDMazeSolver::readWaypointsYAML() {
+  /* Based on simulation or real bot
+    Read waypoints from YAML file & update waypoints vector
+    And configure PID controllers
+  */
+
+  // Get the package's share directory and append the YAML file path
+  std::string package_share_directory =
+      ament_index_cpp::get_package_share_directory("pid_maze_solver");
+
+  std::string waypoint_file_name = "";
+
   switch (scene_number_) {
   case 1: // Simulation
-    // Assign waypoints for Simulation
     RCLCPP_INFO(this->get_logger(), "Welcome to Simulation!");
-    // Waypoints: {dx,dy,dphi}
-    waypoints_ = {
-        {0.27, 0.0, 0.0},      // w1
-        {0.23, -0.07, 0.0},    // w2
-        {0.9, -0.77, 0.0},     // w3
-        {0.532, -0.005, 0.0},  // w4
-        {0.368, 0.315, 0.0},   // w5
-        {0.44, -0.012, 0.0},   // w6
-        {0.446, 0.361, 0.0},   // w7
-        {0.549, -0.0371, 0.0}, // w8
-        {0.718, 0.5381, 0.0},  // w9
-        {0.11, 0.51, 0.0},     // w10
-        {-0.193, 0.322, 0.0},  // w11
-        {0.08, 0.444, 0.0},    // w12
-        {0.225, 0.323, 0.0},   // w13
-        {0.14, 0.617, 0.0}     // w14
-    };
+    waypoint_file_name = "waypoints_sim.yaml";
+
+    /* https://husarion.com/manuals/rosbot-xl/
+    Maximum translational velocity = 0.8 m/s
+    Maximum rotational velocity = 180 deg/s (3.14 rad/s)
+    */
+    max_velocity_ = 0.8;
+    max_ang_velocity_ = 3.14;
+    pid_x_ = PID(2.0, 0.05, 0.3, time_step);
+    pid_y_ = PID(2.0, 0.05, 0.5, time_step);
+    pid_z_ = PID(2.0, 0.01, 0.30, time_step);
     break;
 
   case 2: // CyberWorld
-    // Assign waypoints for CyberWorld
     RCLCPP_INFO(this->get_logger(), "Welcome to CyberWorld!");
-    waypoints_ = {
-        {0.0, 1.0, -1.0},      // w1
-        {0.0, 1.0, 1.0},       // w2
-        {0.0, 1.0, 1.0},       // w3
-        {-1.5708, 1.0, -1.0},  // w4
-        {-1.5708, -1.0, -1.0}, // w5
-        {0.0, -1.0, 1.0},      // w6
-        {0.0, -1.0, 1.0},      // w7
-        {0.0, -1.0, -1.0},     // w8
-        {0.0, 0.0, 0.0}        // Stop
-    };
+    waypoint_file_name = "waypoints_real.yaml";
+
+    /* https://husarion.com/manuals/rosbot-xl/
+    Maximum translational velocity = 0.8 m/s
+    Maximum rotational velocity = 180 deg/s (3.14 rad/s)
+    */
+    max_velocity_ = 0.35;
+    max_ang_velocity_ = 1.5;
+    pid_x_ = PID(2.0, 0.05, 0.3, time_step);
+    pid_y_ = PID(2.0, 0.05, 0.5, time_step);
+    pid_z_ = PID(2.0, 0.01, 0.30, time_step);
+    break;
+
+  case 3: // Simulation Reverse
+    RCLCPP_INFO(this->get_logger(), "Welcome to Simulation Reverse!");
+    waypoint_file_name = "reverse_waypoints_sim.yaml";
+    max_velocity_ = 0.8;
+    max_ang_velocity_ = 3.14;
+    pid_x_ = PID(2.0, 0.05, 0.3, time_step);
+    pid_y_ = PID(2.0, 0.05, 0.5, time_step);
+    pid_z_ = PID(2.0, 0.01, 0.30, time_step);
+    break;
+
+  case 4: // CyberWorld Reverse
+    RCLCPP_INFO(this->get_logger(), "Welcome to CyberWorld Reverse!");
+    waypoint_file_name = "reverse_waypoints_real.yaml";
+    max_velocity_ = 0.35;
+    max_ang_velocity_ = 1.5;
+    pid_x_ = PID(2.0, 0.05, 0.3, time_step);
+    pid_y_ = PID(2.0, 0.05, 0.5, time_step);
+    pid_z_ = PID(2.0, 0.01, 0.30, time_step);
     break;
 
   default:
     RCLCPP_ERROR(this->get_logger(), "Invalid Scene Number: %d", scene_number_);
+  }
+
+  RCLCPP_DEBUG(this->get_logger(), "Waypoint file loaded: %s",
+               waypoint_file_name.c_str());
+
+  std::string yaml_file_path =
+      package_share_directory + "/waypoints/" + waypoint_file_name;
+
+  // Read points from the YAML file
+  try {
+    YAML::Node config = YAML::LoadFile(yaml_file_path);
+
+    if (config["waypoints"]) {
+      for (const auto &wp : config["waypoints"]) {
+        if (wp.size() == 3) { // Ensure it's a valid waypoint
+          waypoints_.push_back(
+              {wp[0].as<double>(), wp[1].as<double>(), wp[2].as<double>()});
+        } else {
+          RCLCPP_WARN(rclcpp::get_logger("WaypointsLoader"),
+                      "Skipping invalid waypoint with incorrect size.");
+        }
+      }
+      RCLCPP_INFO(rclcpp::get_logger("WaypointsLoader"),
+                  "Successfully loaded %zu waypoints.", waypoints_.size());
+    } else {
+      RCLCPP_ERROR(rclcpp::get_logger("WaypointsLoader"),
+                   "No 'waypoints' key found in the YAML file.");
+    }
+  } catch (const YAML::Exception &e) {
+    RCLCPP_ERROR(this->get_logger(), "Failed to load YAML file: %s", e.what());
   }
 }
 
@@ -298,7 +463,7 @@ int main(int argc, char **argv) {
     scene_number = std::atoi(argv[1]);
   }
   // Check if the scene number is valid before creating the node
-  if (scene_number != 1 && scene_number != 2) {
+  if (scene_number < 1 && scene_number > 4) {
     std::cerr << "Error: Invalid Scene Number -- " << scene_number << std::endl;
     rclcpp::shutdown();
     return 1;
